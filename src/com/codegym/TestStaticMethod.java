b16d76652a6a9d1caaae5a8e3b9ed7385b0c9664
sonnguyenece <sonnguyenece@gmail.com>
2020-03-25 22:00:05 +0700

    add static method

---
+++ src/com/codegym/TestStaticMethod.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class TestStaticMethod {
+    public static void main(String args[]) {
+//creating objects
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+//calling display method
+        s1.display();
+        Student.change();//calling change method
+        s2.display();
+        s3.display();
+    }
+}
